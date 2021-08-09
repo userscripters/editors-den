2604a803fbb7818b177f936e5d2517541866a5ba
Oleg Valter <34833340+Oaphi@users.noreply.github.com>
2021-08-09 04:24:42 +0300

    Initial commit

---
+++ dist/headers.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author           Jane Doe <jane@doe.com>
+// @contributors     John Doe <john@doe.com>
+// @description      template repository for userscripters' projects
+// @grant            none
+// @homepage         https://github.com/userscripters/template#readme
+// @match            https//*.stackoverflow.com/*
+// @name             template
+// @namespace        userscripters
+// @source           git+https://github.com/userscripters/template.git
+// @supportURL       https://github.com/userscripters/template/issues
+// @version          1.0.0
+// ==/UserScript==
